5d9c7d4931b37723ed3c3b7679317e41c2c256eb
felix-gustavo <gustavovieira1945@gmail.com>
2021-10-15 19:21:22 -0300

    add roboAction in 5 question

--- Etapa03Atividade01/src/questao5/Test.java
+++ Etapa03Atividade01/src/questao5/Test.java
@@ -5,8 +5,9 @@ import questao5.actions.*;
 public class Test {
     public static void main(String[] args) {
         Experimento experimento = new Experimento();
-        experimento.addAction(new CheckTemp());
+        experimento.addAction(new ApplyChemical());
         experimento.addAction(new IncreaseTemp());
+        experimento.addAction(new CheckTemp());
         experimento.executeTest();
     }
 }
---
+++ Etapa03Atividade01/src/questao5/actions/ApplyChemical.java
@@ -0,0 +1,11 @@
+package questao5.actions;
+
+import questao5.RoboAction;
+
+public class ApplyChemical implements RoboAction{
+
+    @Override
+    public void execute() {
+        System.out.println("Applying chemical...");
+    }
+}
